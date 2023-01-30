265c152f83a6b8140119fd6cc261e451ba098892
Steve Butler <butlersteve.c@gmail.com>
2023-01-29 22:17:58 -0600

    cards rendoring to DOM based on expelled status

--- scripts/main.js
+++ scripts/main.js
@@ -20,8 +20,8 @@ const students = [
   {
     id: 4,
     name: "student 4",
-    house: "Slythering",
-    expelled: true,
+    house: "Slytherin",
+    expelled: false,
   },
   {
     id: 5,
@@ -44,7 +44,54 @@ const students = [
   {
     id: 8,
     name: "student 8",
-    house: "Slythering",
+    house: "Slytherin",
     expelled: true,
   },
 ];
+
+// ### Utility Function
+const renderToDom = (divId, htmlString) => {
+  const displayContainer = document.querySelector(divId);
+  displayContainer.innerHTML = htmlString;
+};
+
+const createCard = (array) => {
+  let activeStudent = "";
+  let expelledStudent = "";
+  let htmlString = "";
+
+  for (const student of array) {
+    if (!student.expelled) {
+      activeStudent += ` <div class="card w-25 d-flex flex-row ">
+      <div class="color--${student.house}1"></div>
+      
+      <div class="card-body">
+         <h5 class="card-title">${student.name}</h5>
+         <p class="card-text">${student.house}</p>
+         <a id="expelled" class="btn btn-sm btn-danger">Expell</a>
+      </div>
+     
+      
+   </div>
+   `;
+    } else {
+      expelledStudent += `<div class="card w-25">
+        <div class="card-body">
+          <h5 class="card-title">EXPELLED</h5>
+          <p class="card-text">${student.name}</p>
+        </div>
+  </div> `;
+    }
+  }
+
+  if (activeStudent.length > 0) {
+    htmlString = activeStudent;
+    renderToDom("#firstYearsContainer", htmlString);
+  }
+  if (expelledStudent.length > 0) {
+    htmlString = expelledStudent;
+    renderToDom("#mortysArmyContainer", htmlString);
+  }
+};
+
+createCard(students);
